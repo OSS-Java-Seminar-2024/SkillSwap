494b1d63392c9995e1c488a70cb5aedc9935b58e
Domina718 <92520131+Domina718@users.noreply.github.com>
2024-11-25 00:51:08 +0100

    Project setup

---
+++ SkillSwapApp/src/main/java/org/example/skillSwap/SkillSwapApplication.java
@@ -0,0 +1,13 @@
+package org.example.skillSwap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSwapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SkillSwapApplication.class, args);
+
+        System.out.println("Hello world!");
+    }
+}
